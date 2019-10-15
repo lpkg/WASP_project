e2fe835aa7d6b1ab163eee6c1488ca3d6ea784f6
minaf <minaferizbegovic@gmail.com>
2019-10-15 17:54:02 +0200

    Estimate Lambda function added

---
+++ estimate_lambda.py
@@ -0,0 +1,27 @@
+#!python
+import numpy as np
+from numpy.linalg import svd
+from epipolar import epipolar
+from numpy import linalg as LA
+
+def estimate_lambda(Ws, list_F):
+    """Estimate some initial projective depth given the measurement matrix Ws
+       and Fundamental matrices.
+    """
+
+    n = Ws.shape[0]/3
+    m = Ws.shape[1]
+    Lambda = np.ones((n, m))
+    for i in range(n-1):
+        j = i+1
+        F = list_F[i][0]
+        e = epipolar(F)
+        for p in list_F[i][1]:
+            p = p - 1
+            q_ip = Ws[3*i:3*i+3, p]
+            q_jp = Ws[3*j:3*j+3, p]
+            np.cross(e, q_ip)
+            tmp1 = LA.norm(np.cross(e, q_ip), 2)**2
+            tmp2 = np.dot(np.matmul(F.T, q_jp), np.cross(e, q_ip))
+            Lambda[j, p] = Lambda[i, p]/tmp2.sum()*tmp1
+    return Lambda
